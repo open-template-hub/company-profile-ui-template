5b39651fe5c8ce3d6ae12bc4182a1d2b63072e18
furknyavuz <furknyavuz@gmail.com>
2021-08-28 00:55:29 +0300

    remove quotes

--- src/app/page/raw-landing-layout/maintenance/maintenance.component.ts
+++ src/app/page/raw-landing-layout/maintenance/maintenance.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ThemeService } from '../../../service/theme/theme.service';
-import { QUOTES } from '../../../util/constant';
 
 @Component( {
   selector: 'app-maintenance',
   templateUrl: './maintenance.component.html',
   styleUrls: [ './maintenance.component.scss', '../raw-landing-layout.component.scss' ]
 } )
-export class MaintenanceComponent implements OnInit {
+export class MaintenanceComponent {
 
   brand = {
     brandLogo: '',
-  };
-
-  selectedQuote = {
-    quote: '',
-    author: ''
   };
 
   constructor(
@@ -25,13 +19,5 @@ export class MaintenanceComponent implements OnInit {
       private themeService: ThemeService ) {
 
     this.brand = this.themeService.brand;
-  }
-
-  ngOnInit(): void {
-    const array = new Uint32Array( 1 );
-    window.crypto.getRandomValues( array );
-
-    const rand = array[0] % 110;
-    this.selectedQuote = QUOTES[ rand ];
   }
 }
--- src/app/page/raw-landing-layout/not-found/not-found.component.ts
+++ src/app/page/raw-landing-layout/not-found/not-found.component.ts
@@ -1,35 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ThemeService } from '../../../service/theme/theme.service';
-import { QUOTES } from '../../../util/constant';
 
 @Component( {
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: [ './not-found.component.scss', '../raw-landing-layout.component.scss' ]
 } )
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
 
   brand = {
     brandLogo: '',
-  };
-
-  selectedQuote = {
-    quote: '',
-    author: ''
   };
 
   constructor(
       public router: Router,
       private themeService: ThemeService ) {
     this.brand = this.themeService.brand;
-  }
-
-  ngOnInit(): void {
-    const array = new Uint32Array( 1 );
-    window.crypto.getRandomValues( array );
-
-    const rand = array[ 0 ] % 110;
-    this.selectedQuote = QUOTES[ rand ];
   }
 }

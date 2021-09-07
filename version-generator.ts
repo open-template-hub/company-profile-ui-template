a885f0fe7b03a62bdeb40d1317ca86ca99809df8
Furkan Yavuz <furknyavuz@gmail.com>
2021-09-07 05:58:51 +0300

    version generator update

--- version-generator.ts
+++ version-generator.ts
@@ -1,1 +1,32 @@
-console.log( 'export const version = \'' + require( './package.json' ).version + '\';' )
+const version = require( './package.json' ).version;
+const jsdom = require( 'jsdom' );
+const { JSDOM } = jsdom;
+const fs = require( 'fs' );
+
+console.log( 'export const version = \'' + version + '\';' );
+
+JSDOM.fromFile( './src/index.html' ).then( dom => {
+
+  const urlVersion = version.split( '.' ).join( '_' );
+
+  const favicons = [
+    dom.window.document.querySelector( '[rel="shortcut icon"]' ),
+    dom.window.document.querySelector( '[rel="manifest"]' ),
+    dom.window.document.querySelector( '[rel="mask-icon"]' ),
+  ];
+
+  for ( const favicon of favicons ) {
+    let hrefWithVersion;
+    if ( favicon.getAttribute( 'href' ).includes( '?v=' ) ) {
+      hrefWithVersion = favicon.getAttribute( 'href' ).substring( 0, favicon.getAttribute( 'href' ).indexOf( '?v=' ) + 3 ) + urlVersion;
+    } else {
+      hrefWithVersion = favicon.getAttribute( 'href' ) + '?v=' + urlVersion;
+    }
+
+    favicon.setAttribute( 'href', hrefWithVersion );
+  }
+
+  fs.writeFile( './src/index.html', dom.window.document.documentElement.outerHTML, () => {
+    // Intentionally blank
+  } );
+} );

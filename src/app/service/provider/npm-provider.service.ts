f56ed2608f95c1ac40baf8c84b6b8ef8c39d887c
Fatih Turker <fatihturker35@gmail.com>
2021-09-12 15:47:13 +0300

    Remove unused import

--- src/app/service/provider/npm-provider.service.ts
+++ src/app/service/provider/npm-provider.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { NpmPackages, OthStartDate } from "src/app/data/constant";
 import { environment } from "../../../environments/environment";

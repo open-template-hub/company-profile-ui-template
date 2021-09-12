2081de1204d9685ddc830a3e603ae7e3d721fc6c
Mert Saraç <mertsarac@yandex.com>
2021-09-12 20:19:43 +0300

    added expandable pricing page

--- src/app/data/product/product.data.ts
+++ src/app/data/product/product.data.ts
@@ -89,6 +89,52 @@ export const PRODUCT_LINES: ProductLine[] = [
               fontSize: '1em',
               featureHeight: '10em'
             }
+          },
+          {
+            title: 'STARTER',
+            price: {
+              currency: '$',
+              value: '9',
+              subscriptionTime: 'mo.'
+            },
+            features: [ 'Up to 5 users', 'Maximum 100 photos/mo.', '50 queries' ],
+            styles: {
+              width: '300px',
+              height: '400px',
+              fontSize: '1em',
+              featureHeight: '10em'
+            }
+          },
+          {
+            title: 'BASIC',
+            ribbon: { theme: 'blue', text: 'DEAL' },
+            price: {
+              currency: '$',
+              value: '49',
+              subscriptionTime: 'mo.'
+            },
+            features: [ 'Up to 10 users', 'Maximum 100 photos/mo.', '500 queries' ],
+            styles: {
+              width: '370px',
+              height: '520px',
+              fontSize: '1.2em',
+              featureHeight: '13em'
+            }
+          },
+          {
+            title: 'PRO',
+            price: {
+              currency: '$',
+              value: '99',
+              subscriptionTime: 'mo.'
+            },
+            features: [ 'Unlimited users', 'Unlimited photos/mo.', '5000 queries' ],
+            styles: {
+              width: '300px',
+              height: '400px',
+              fontSize: '1em',
+              featureHeight: '10em'
+            }
           }
         ]
       },

8f2d071c5e5b782e424c8f0a66e2baaced5c8622
Fatih Turker <fatihturker35@gmail.com>
2021-09-13 20:36:33 +0300

    Add dynamic command line generation with animation

--- src/app/component/animated-code-editor/animated-code-editor.component.ts
+++ src/app/component/animated-code-editor/animated-code-editor.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ComponentFactory,
+  ComponentFactoryResolver,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { CommandType } from 'src/app/enum/command-type.enum';
+import { CommandLineComponent } from '../command-line/command-line.component';
 
 @Component({
   selector: 'app-animated-code-editor',
   templateUrl: './animated-code-editor.component.html',
-  styleUrls: ['./animated-code-editor.component.scss']
+  styleUrls: ['./animated-code-editor.component.scss'],
 })
 export class AnimatedCodeEditorComponent implements OnInit {
+  constructor(private resolver: ComponentFactoryResolver) {}
 
-  constructor() { }
+  @ViewChild('codeEditorContent', { static: true, read: ViewContainerRef })
+  codeEditorContentRef: ViewContainerRef;
+
+  factory: ComponentFactory<CommandLineComponent>;
+  index: number = 0;
+
+  commandLines = [
+    {
+      command: 'open-template-hub-server-generator',
+      type: CommandType.Request,
+    },
+    { command: '1) Payment Server', type: CommandType.Response },
+    { command: '2) Auth Server', type: CommandType.Response },
+  ];
 
   ngOnInit(): void {
+    this.codeEditorContentRef.clear();
+    this.index = 0;
+    this.factory = this.resolver.resolveComponentFactory(CommandLineComponent);
+    this.animationTrigger();
   }
 
+  createComponent = (commandLine: any) => {
+    if (this.index < this.commandLines.length) {
+      this.index++;
+      const componentRef = this.codeEditorContentRef.createComponent(
+        this.factory
+      );
+      componentRef.instance.command = commandLine.command;
+      componentRef.instance.type = commandLine.type;
+      componentRef.instance.animationComplete = this.animationTrigger;
+    }
+  };
+
+  animationTrigger = () => {
+    console.log('Animation triggered with index: ', this.index);
+    this.createComponent(this.commandLines[this.index]);
+  };
 }
--- src/app/component/command-line/command-line.component.ts
+++ src/app/component/command-line/command-line.component.ts
@@ -10,7 +10,28 @@ export class CommandLineComponent implements OnInit {
   @Input() command: string = '';
   @Input() type: CommandType = CommandType.Request;
 
+  defaultCallback = () => {
+    console.log('Animation completed for ', this.command);
+  };
+
+  @Input() animationComplete?: any = this.defaultCallback;
+
+  animatedText: string = '';
+  counter: number = 0;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.animateCommand();
+  }
+
+  animateCommand = () => {
+    if (this.counter < this.command.length) {
+      this.animatedText += this.command.charAt(this.counter);
+      this.counter++;
+      setTimeout(this.animateCommand, 30);
+    } else {
+      this.animationComplete();
+    }
+  };
 }

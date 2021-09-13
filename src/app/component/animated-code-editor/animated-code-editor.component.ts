ef2f0c8fb1dac58201b3cc305f750ba55575121c
Fatih Turker <fatihturker35@gmail.com>
2021-09-13 13:52:47 +0300

    Implement initial terminal card

---
+++ src/app/component/animated-code-editor/animated-code-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-code-editor',
+  templateUrl: './animated-code-editor.component.html',
+  styleUrls: ['./animated-code-editor.component.scss']
+})
+export class AnimatedCodeEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

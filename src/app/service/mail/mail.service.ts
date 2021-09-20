8ff81fc7042fe25bb6314de038ccfc7c5247a9fc
Fatih Turker <fatihturker35@gmail.com>
2021-09-20 20:00:42 +0300

    Add contact us mail integration

---
+++ src/app/service/mail/mail.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MailService {
+  constructor(private http: HttpClient) {}
+
+  sendContactUsMail(payload: any) {
+    var contactUs = {
+      params: payload,
+    };
+    return this.http.post<any>(`${environment.serverUrl}/mail/contact`, {
+      contactUs,
+    });
+  }
+}

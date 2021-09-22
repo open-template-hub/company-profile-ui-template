eb74b79ec666bdf56fb4714c502244c571618ebf
Fatih Turker <fatihturker35@gmail.com>
2021-09-22 21:46:39 +0300

    Implement contact us sent case

--- src/app/service/mail/mail.service.ts
+++ src/app/service/mail/mail.service.ts
@@ -14,9 +14,6 @@ export class MailService {
       params: payload,
     };
 
-    this.http.post<any>( `${ environment.serverUrl }/mail/contact`, { contactUs } )
-    .subscribe( ( data ) => {
-      console.log( data );
-    } );
+    return this.http.post<any>( `${ environment.serverUrl }/mail/contact`, { contactUs } );
   }
 }

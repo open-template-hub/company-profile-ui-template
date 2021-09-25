093dbfbb27a64fdd449ee15e818211756a5ff5d9
Mert Saraç <mertsarac@yandex.com>
2021-09-26 02:02:47 +0300

    changed buttons in price-plan

--- src/app/component/price-plan/price-plan.component.ts
+++ src/app/component/price-plan/price-plan.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { URLS } from '../../data/constant';
 import { Product } from '../../model/product/product.model';
+import { ButtonType, ButtonClass } from '../../component/button/button.component';
 
 @Component( {
   selector: 'app-price-plan',
@@ -9,6 +10,8 @@ import { Product } from '../../model/product/product.model';
   styleUrls: [ './price-plan.component.scss' ]
 } )
 export class PricePlanComponent {
+  ButtonType = ButtonType
+  ButtonClass = ButtonClass
   URLS = URLS;
 
   @Input() product: Product;

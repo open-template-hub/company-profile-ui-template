ba5702bfcf6b433eb942b86053922593f559c0ff
Mert Saraç <mertsarac@yandex.com>
2021-09-26 01:43:47 +0300

    changed buttons in product-page

--- src/app/page/landing-layout/product-page/product-page.component.ts
+++ src/app/page/landing-layout/product-page/product-page.component.ts
@@ -9,6 +9,7 @@ import { PARTNERS } from '../../../data/partner/partner.data';
 import { PRODUCT_LINES, SERVICES } from '../../../data/product/product.data';
 import { Product, ProductLine } from '../../../model/product/product.model';
 import { ProductService } from '../../../service/product/product.service';
+import { ButtonType, ButtonClass } from '../../../component/button/button.component';
 
 @Component( {
   selector: 'app-product-page',
@@ -16,6 +17,8 @@ import { ProductService } from '../../../service/product/product.service';
   styleUrls: [ './product-page.component.scss' ],
 } )
 export class ProductPageComponent implements OnInit, OnDestroy {
+  ButtonType = ButtonType
+  ButtonClass = ButtonClass
 
   URLS = URLS;
   TESTIMONIALS = TESTIMONIALS;

f56e842ab62ee452d9a9915ce6132d55b4851d5e
Furkan Yavuz <furknyavuz@gmail.com>
2021-10-01 18:32:22 +0300

    run service worker immidiately

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -195,7 +195,7 @@ FullCalendarModule.registerPlugins( [
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     })
     /*HttpClientInMemoryWebApiModule.forRoot(
      InMemoryDataService, { dataEncapsulation: false } )*/

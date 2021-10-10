2b3479ec5b5d4a0f6f5bf6b21ea0ac02606a4e08
Fatih Turker <fatihturker35@gmail.com>
2021-10-10 16:04:38 +0300

    Introduce Image Loaders

--- src/app/component/dropdown-menu/dropdown-menu.component.ts
+++ src/app/component/dropdown-menu/dropdown-menu.component.ts
@@ -1,15 +1,20 @@
-import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { URLS } from '../../data/constant';
-import { ProductLine } from '../../model/product/product.model';
+import { Product, ProductLine } from '../../model/product/product.model';
 import { UtilService } from '../../service/util/util.service';
 
-@Component( {
+@Component({
   selector: 'app-dropdown-menu',
   templateUrl: './dropdown-menu.component.html',
-  styleUrls: [ './dropdown-menu.component.scss' ]
-} )
+  styleUrls: ['./dropdown-menu.component.scss'],
+})
 export class DropdownMenuComponent {
-
   URLS = URLS;
 
   @Input() isDropdownOpen = false;
@@ -23,38 +28,42 @@ export class DropdownMenuComponent {
 
   closeDropdownInternalClicked = false;
 
-  @ViewChild( 'toggleButton' ) toggleButton: ElementRef;
-  @ViewChild( 'dropdownContent' ) dropdownContent: ElementRef;
+  @ViewChild('toggleButton') toggleButton: ElementRef;
+  @ViewChild('dropdownContent') dropdownContent: ElementRef;
 
-  constructor( private utilService: UtilService ) {
+  constructor(private utilService: UtilService) {
     this.calculatedColumns = this.minimumColumns;
     this.calculatedRows = this.minimumRows;
   }
 
-  @HostListener( 'window:resize', [ '$event' ] )
+  @HostListener('window:resize', ['$event'])
   onResize() {
-
-    if ( this.isDropdownOpen && window.innerWidth > 999 ) {
+    if (this.isDropdownOpen && window.innerWidth > 999) {
       let wantedColumns = this.minimumColumns;
 
-      while ( wantedColumns * 330 > window.innerWidth - 240 ) {
+      while (wantedColumns * 330 > window.innerWidth - 240) {
         wantedColumns = wantedColumns - 2;
       }
 
       this.calculatedColumns = wantedColumns;
 
-      if ( this.calculatedColumns < this.minimumColumns ) {
-        this.calculatedRows = this.minimumRows - ( this.minimumColumns - this.calculatedColumns + 2 );
+      if (this.calculatedColumns < this.minimumColumns) {
+        this.calculatedRows =
+          this.minimumRows - (this.minimumColumns - this.calculatedColumns + 2);
       } else {
         this.calculatedRows = this.minimumRows;
       }
     }
   }
 
+  setImageLoaded = (product: Product) => {
+    product.imageLoaded = true;
+  };
+
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
 
-    if ( this.isDropdownOpen ) {
+    if (this.isDropdownOpen) {
       this.onResize();
     }
   }
@@ -66,8 +75,8 @@ export class DropdownMenuComponent {
 
   closeDropDownInternal() {
     this.closeDropdownInternalClicked = true;
-    this.utilService.delay( 500 ).then( () => {
+    this.utilService.delay(500).then(() => {
       this.closeDropdown();
-    } );
+    });
   }
 }

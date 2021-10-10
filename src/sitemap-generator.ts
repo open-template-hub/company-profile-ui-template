91578048ad45bef54bde47ea42ff06fff21eb262
Furkan Yavuz <furknyavuz@gmail.com>
2021-10-10 22:07:43 +0300

    sitemap config added

--- src/sitemap-generator.ts
+++ src/sitemap-generator.ts
@@ -1,6 +1,6 @@
-var url = 'https://opentemplatehub.com';
+const url = 'https://opentemplatehub.com';
 
-var exposedRoutemap = [
+const exposedRoutemap = [
   {
     path: '',
     children: [
@@ -54,12 +54,12 @@ var exposedRoutemap = [
   },
 ];
 
-var today = new Date().toISOString().split('T')[0];
+const today = new Date().toISOString().split('T')[0];
 
 function getSitemap(path, routes) {
-  var sitemapStr = '';
+  let sitemapStr = '';
   routes.forEach((r) => {
-    var nUrl = path + r.path;
+    const nUrl = path + r.path;
     sitemapStr +=
       '<url><loc>' + nUrl + '</loc><lastmod>' + today + '</lastmod></url>';
     if (r.children && r.children.length > 0) {
@@ -69,7 +69,7 @@ function getSitemap(path, routes) {
   return sitemapStr;
 }
 
-var sitemap =
+let sitemap =
   '<?xml version="1.0" encoding="UTF-8"?>' +
   '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 sitemap += getSitemap(url, exposedRoutemap);

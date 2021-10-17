a07d8d0efd983e6f98deab0806620b22b19320fc
Fatih Turker <fatihturker35@gmail.com>
2021-10-17 18:27:43 +0300

    Fix sonar issues

--- src/app/component/card/feature-card/feature-card.component.ts
+++ src/app/component/card/feature-card/feature-card.component.ts
@@ -1,22 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Feature } from 'src/app/model/feature/feature.model';
 
 @Component({
   selector: 'app-feature-card',
   templateUrl: './feature-card.component.html',
-  styleUrls: ['./feature-card.component.scss']
+  styleUrls: ['./feature-card.component.scss'],
 })
-export class FeatureCardComponent implements OnInit {
-
+export class FeatureCardComponent {
   @Input() feature: Feature = {
     title: '',
     description: '',
     icon: '',
   } as Feature;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // Intentionally blank
   }
-
 }

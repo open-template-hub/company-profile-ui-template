9e7ee029b1489c9660a7d2d55224a234c4eff511
Furkan Yavuz <furknyavuz@gmail.com>
2021-10-19 19:36:35 +0300

    update top-svg by design

--- src/app/page/landing-layout/landing-layout.component.ts
+++ src/app/page/landing-layout/landing-layout.component.ts
@@ -12,7 +12,9 @@ export class LandingLayoutComponent {
 
   loading = false;
 
-  topSvgPath = 'M 0,240 1000,110 V 0 H 0 Z';
+  private DEFAULT_SVG_PATH = 'M 0,240 1000,110 V 0 H 0 Z';
+
+  topSvgPath = this.DEFAULT_SVG_PATH;
 
   constructor( private loadingService: LoadingService, private themeService: ThemeService ) {
 
@@ -24,19 +26,21 @@ export class LandingLayoutComponent {
   private setDesign( themeDesignSetting: string ) {
 
     switch ( themeDesignSetting ) {
-      case ThemeDesignSettings.sharp : {
-        this.topSvgPath = 'M 0,240 1000,110 V 0 H 0 Z';
+      case ThemeDesignSettings.sharp :
+        this.topSvgPath = this.DEFAULT_SVG_PATH;
         break;
-      }
-      case ThemeDesignSettings.leaf : {
-        this.topSvgPath = 'm 320,200 c 313.8384,-42.50051 365.51464,-49.11691 680,-90 V 0 H 0 v 0 c 1.1136394,46.09311 20.88663,241.94628 320,200 z';
+      case ThemeDesignSettings.leaf :
+        this.topSvgPath = this.DEFAULT_SVG_PATH;
         break;
-      }
       case ThemeDesignSettings.counterBorders :
+        this.topSvgPath = this.DEFAULT_SVG_PATH;
+        break;
       case ThemeDesignSettings.round :
+        this.topSvgPath = this.DEFAULT_SVG_PATH;
+        break;
       case DEFAULT_THEME :
       default : {
-        this.topSvgPath = 'M 0,240 1000,110 V 0 H 0 Z';
+        this.topSvgPath = this.DEFAULT_SVG_PATH;
         break;
       }
     }

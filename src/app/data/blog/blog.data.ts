8edd3dcfb548b9f866c47a552f0361b7d5662dc3
Furkan Yavuz <furknyavuz@gmail.com>
2021-10-26 10:35:45 +0300

    blog holder page ui make up

--- src/app/data/blog/blog.data.ts
+++ src/app/data/blog/blog.data.ts
@@ -10,23 +10,5 @@ export const BLOG: Blog[] = [
     excerpt: 'We, as Open Template Hub contributors, keep developing new functionalities and capabilities every week. You can create your microservices or create your mobile and web applications which are integrated with those microservices, that means you can create your own business ground up through Open Template Hub extremely fast..',
     author: EMPLOYEES[ 1 ],
     tag: 'tech',
-  },
-  {
-    key: 'what-is-open-template-hub-2',
-    url: 'blog-1.md',
-    date: '21 Oct 2021',
-    title: 'What is Open Template Hub?',
-    excerpt: 'We, as Open Template Hub contributors, keep developing new functionalities and capabilities every week. You can create your microservices or create your mobile and web applications which are integrated with those microservices, that means you can create your own business ground up through Open Template Hub extremely fast..',
-    author: EMPLOYEES[ 0 ],
-    tag: 'daily',
-  },
-  {
-    key: 'what-is-open-template-hub-3',
-    url: 'blog-1.md',
-    date: '21 Oct 2021',
-    title: 'What is Open Template Hub?',
-    excerpt: 'We, as Open Template Hub contributors, keep developing new functionalities and capabilities every week. You can create your microservices or create your mobile and web applications which are integrated with those microservices, that means you can create your own business ground up through Open Template Hub extremely fast..',
-    author: EMPLOYEES[ 2 ],
-    tag: 'life',
   }
 ];

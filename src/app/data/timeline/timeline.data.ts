2cda5deea729a3f088cdfc07b781c8d04e03ce58
Furkan Yavuz <furknyavuz@gmail.com>
2021-10-26 14:00:00 +0300

    timeline data update

--- src/app/data/timeline/timeline.data.ts
+++ src/app/data/timeline/timeline.data.ts
@@ -1,92 +1,18 @@
 import { TimeLineItem } from '../../model/timeline/timeline.model';
-import { ThemeColorSettings } from '../constant';
-
-const DEFAULT_PARAGRAPH = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.';
 
 export const OTH_TIME_LINE: TimeLineItem[] = [
   {
     title: 'Initial Commit',
     date: '27 Feb 2020',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/server/auth-server-logo.min.png',
+    paragraph: 'We committed first lines of the code of the Open Template Hub at the beginning of 2020. We continued to improve our products throughout the pandemic.',
+    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/timeline/ui/company-profile-ui-timeline-1.png',
     footerText: '#initial-commit',
-  },
-  {
-    title: 'Web UI Template',
-    date: '7 Mar 2020',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/ui/web-ui-logo.min.png',
-    footerText: '#initial-ui',
-  },
-  {
-    title: 'Business Logic Server Template',
-    date: '23 May 2020',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/server/business-logic-server-logo.min.png',
-    footerText: '#business-logic',
-  },
-  {
-    title: 'Orchestration Server',
-    date: '22 May 2020',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/server/orchestration-server-logo.min.png',
-    theme: ThemeColorSettings.yellow,
-    footerText: '#premium',
-  },
-  {
-    title: 'Payment Server Template',
-    date: '28 May 2020',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/server/payment-server-logo.min.png',
-    footerText: '#payment',
   },
   {
     title: 'Hello Npm',
     date: '14 Jun 2020',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/generator/server-generator-logo.min.png',
-    footerText: '#hellonpm'
-  },
-  {
-    title: 'File Storage Server Template',
-    date: '28 Jun 2020',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/server/file-storage-server-logo.min.png',
-    footerText: '#file-storage',
-  },
-  {
-    title: 'Analytics Server Template',
-    date: '27 Dec 2020',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/server/analytics-server-logo.min.png',
-    footerText: '#analytics',
-  },
-  {
-    title: 'Mobile Ui Template',
-    date: '24 Jan 2021',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/ui/mobile-ui-logo.min.png',
-    footerText: '#mobile-ui',
-  },
-  {
-    title: 'Ui Generator',
-    date: '06 Feb 2021',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/generator/server-generator-logo.min.png',
-    footerText: '#generator',
-  },
-  {
-    title: 'Company Profile Ui Template',
-    date: '15 Aug 2021',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/ui/web-ui-logo.min.png',
-    footerText: '#company',
-  },
-  {
-    title: 'Mail Server Template',
-    date: '16 Sep 2021',
-    paragraph: DEFAULT_PARAGRAPH,
-    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/min/logo/server/mail-server-logo.min.png',
-    footerText: '#mail',
+    paragraph: 'Open Template Hub joined to Npm with Server Generator cli package. As of October 2021 reached to 3.5k downloads.',
+    img: 'https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/timeline/ui/company-profile-ui-timeline-2.png',
+    footerText: '#hello-npm'
   }
 ];

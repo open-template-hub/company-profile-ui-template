eebf7eeae1a99c172eaa196d8ad5ddc5e281ca79
Mert Saraç <mertsarac@yandex.com>
2021-10-30 22:28:00 +0300

    fixed sonar issues

--- src/app/component/nav/bottom-nav/bottom-nav.component.ts
+++ src/app/component/nav/bottom-nav/bottom-nav.component.ts
@@ -2,7 +2,6 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { PRODUCT_LINES, SERVICES } from 'src/app/data/product/product.data';
 import { URLS } from '../../../data/navigation/navigation.data';
-import { AuthToken } from '../../../model/auth/auth-token.model';
 import { ProductLine } from '../../../model/product/product.model';
 import { LoadingService } from '../../../service/loading/loading.service';
 

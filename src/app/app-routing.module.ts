4bc8488b2cc723a0a87d13e79a89f823a399ee30
Mert Saraç <mertsarac@yandex.com>
2021-10-30 03:15:19 +0300

    edited app-routing.module and app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { environmentCommon } from '../environments/environment-common';
 import { NAVIGATIONS } from './data/navigation/navigation.data';
-import { AuthGuard } from './guard/auth/auth.guard';
-import { PublicProfileGuard } from './guard/public-profile/public-profile.guard';
 import { NavigationInterceptor } from './interceptor/navigation/navigation.interceptor';
-import { CreateEventPageComponent } from './page/dashboard-layout/create-event-page/create-event-page.component';
-import { DashboardLayoutComponent } from './page/dashboard-layout/dashboard-layout.component';
-import { DashboardPageComponent } from './page/dashboard-layout/dashboard-page/dashboard-page.component';
-import { EventPageComponent } from './page/dashboard-layout/event-page/event-page.component';
-import { LearnPageComponent } from './page/dashboard-layout/learn-page/learn-page.component';
-import { MyProfilePageComponent } from './page/dashboard-layout/my-profile-page/my-profile-page.component';
-import { PublicProfilePageComponent } from './page/dashboard-layout/public-profile-page/public-profile-page.component';
 import { AboutUsPageComponent } from './page/landing-layout/about-us-page/about-us-page.component';
 import { BlogContentPageComponent } from './page/landing-layout/blog-page/blog-content-page/blog-content-page.component';
 import { BlogPageComponent } from './page/landing-layout/blog-page/blog-page.component';
@@ -20,10 +10,6 @@ import { ContactUsPageComponent } from './page/landing-layout/contact-us-page/co
 import { CustomersPageComponent } from './page/landing-layout/customers-page/customers-page.component';
 import { DocsContentPageComponent } from './page/landing-layout/docs-page/docs-content-page/docs-content-page.component';
 import { DocsPageComponent } from './page/landing-layout/docs-page/docs-page.component';
-import { ForgetPasswordPageComponent } from './page/landing-layout/form-pages/forget-password-page/forget-password-page.component';
-import { LoginPageComponent } from './page/landing-layout/form-pages/login-page/login-page.component';
-import { ResetPasswordPageComponent } from './page/landing-layout/form-pages/reset-password-page/reset-password-page.component';
-import { SignUpPageComponent } from './page/landing-layout/form-pages/sign-up-page/sign-up-page.component';
 import { HomePageComponent } from './page/landing-layout/home-page/home-page.component';
 import { LandingLayoutComponent } from './page/landing-layout/landing-layout.component';
 import { PartnersPageComponent } from './page/landing-layout/partners-page/partners-page.component';
@@ -34,13 +20,8 @@ import { PricingPageComponent } from './page/landing-layout/pricing-page/pricing
 import { ProductPageComponent } from './page/landing-layout/product-page/product-page.component';
 import { MaintenancePageComponent } from './page/landing-layout/raw-content-pages/maintenance-page/maintenance-page.component';
 import { NotFoundPageComponent } from './page/landing-layout/raw-content-pages/not-found-page/not-found-page.component';
-import { SignUpSuccessPageComponent } from './page/landing-layout/raw-content-pages/sign-up-success-page/sign-up-success-page.component';
-import { VerifyAccountPageComponent } from './page/landing-layout/raw-content-pages/verify-account-page/verify-account-page.component';
 import { SitemapPageComponent } from './page/landing-layout/sitemap-page/sitemap-page.component';
 import { StatusPageComponent } from './page/landing-layout/status-page/status-page.component';
-import { EditThemePageComponent } from './page/settings-layout/edit-theme-page/edit-theme-page.component';
-import { SettingsLayoutComponent } from './page/settings-layout/settings-layout.component';
-import { CallbackPageComponent } from './page/splash-layout/callback-page/callback-page.component';
 import { ExternalRedirectPageComponent } from './page/splash-layout/external-redirect-page/external-redirect-page.component';
 import { SplashLayoutComponent } from './page/splash-layout/splash-layout.component';
 
@@ -65,16 +46,6 @@ const routes: Routes = [
         canActivate: [ NavigationInterceptor ],
       },
       {
-        path: NAVIGATIONS.signupSuccess.url,
-        component: SignUpSuccessPageComponent,
-        canActivate: [ NavigationInterceptor ],
-      },
-      {
-        path: NAVIGATIONS.verifyAccount.url,
-        component: VerifyAccountPageComponent,
-        canActivate: [ NavigationInterceptor ],
-      },
-      {
         path: NAVIGATIONS.cookiePolicy.url,
         component: CookiePolicyPageComponent,
         canActivate: [ NavigationInterceptor ],
@@ -92,16 +63,6 @@ const routes: Routes = [
       {
         path: NAVIGATIONS.about.url,
         component: AboutUsPageComponent,
-        canActivate: [ NavigationInterceptor ],
-      },
-      {
-        path: NAVIGATIONS.signup.url,
-        component: SignUpPageComponent,
-        canActivate: [ NavigationInterceptor ],
-      },
-      {
-        path: NAVIGATIONS.login.url,
-        component: LoginPageComponent,
         canActivate: [ NavigationInterceptor ],
       },
       {
@@ -130,16 +91,6 @@ const routes: Routes = [
         canActivate: [ NavigationInterceptor ],
       },
       {
-        path: NAVIGATIONS.forgetPassword.url,
-        component: ForgetPasswordPageComponent,
-        canActivate: [ NavigationInterceptor ],
-      },
-      {
-        path: NAVIGATIONS.resetPassword.url,
-        component: ResetPasswordPageComponent,
-        canActivate: [ NavigationInterceptor ],
-      },
-      {
         path: NAVIGATIONS.contactUs.url,
         component: ContactUsPageComponent,
         canActivate: [ NavigationInterceptor ],
@@ -152,12 +103,6 @@ const routes: Routes = [
       {
         path: NAVIGATIONS.sitemap.url,
         component: SitemapPageComponent,
-        canActivate: [ NavigationInterceptor ],
-      },
-      {
-        path: NAVIGATIONS.u.url + '/' + ':username',
-        component: PublicProfilePageComponent,
-        data: { isPublic: true },
         canActivate: [ NavigationInterceptor ],
       },
       {
@@ -183,114 +128,9 @@ const routes: Routes = [
     ],
   },
   {
-    path: NAVIGATIONS.callback.url,
-    component: SplashLayoutComponent,
-    children: [
-      {
-        path: NAVIGATIONS.dribbble.url,
-        component: CallbackPageComponent,
-        data: { oauth: environmentCommon.website.dribbble },
-      },
-      {
-        path: NAVIGATIONS.facebook.url,
-        component: CallbackPageComponent,
-        data: { oauth: environmentCommon.website.facebook },
-      },
-      {
-        path: NAVIGATIONS.github.url,
-        component: CallbackPageComponent,
-        data: { oauth: environmentCommon.website.github },
-      },
-      {
-        path: NAVIGATIONS.google.url,
-        component: CallbackPageComponent,
-        data: { oauth: environmentCommon.website.google },
-      },
-      {
-        path: NAVIGATIONS.linkedin.url,
-        component: CallbackPageComponent,
-        data: { oauth: environmentCommon.website.linkedin },
-      },
-      {
-        path: NAVIGATIONS.reddit.url,
-        component: CallbackPageComponent,
-        data: { oauth: environmentCommon.website.reddit },
-      },
-      {
-        path: NAVIGATIONS.twitch.url,
-        component: CallbackPageComponent,
-        data: { oauth: environmentCommon.website.twitch },
-      },
-      {
-        path: NAVIGATIONS.twitter.url,
-        component: CallbackPageComponent,
-        data: { oauth: environmentCommon.website.twitter },
-      },
-      {
-        path: NAVIGATIONS.stripe.url,
-        component: CallbackPageComponent,
-        data: { payment: environmentCommon.website.stripe },
-      },
-    ],
-  },
-  {
     path: NAVIGATIONS.external.url,
     component: SplashLayoutComponent,
     children: [ { path: '', component: ExternalRedirectPageComponent } ],
-  },
-  {
-    path: NAVIGATIONS.dashboard.url,
-    component: DashboardLayoutComponent,
-    children: [
-      { path: '', component: DashboardPageComponent, canActivate: [ AuthGuard ] },
-      {
-        path: NAVIGATIONS.myProfile.url,
-        component: MyProfilePageComponent,
-        canActivate: [ AuthGuard ],
-      },
-      {
-        path: NAVIGATIONS.createEvent.url,
-        component: CreateEventPageComponent,
-        canActivate: [ AuthGuard ],
-      },
-      {
-        path: NAVIGATIONS.learn.url,
-        component: LearnPageComponent,
-        canActivate: [ AuthGuard ],
-      },
-      {
-        path: NAVIGATIONS.event.url,
-        component: EventPageComponent,
-        canActivate: [ AuthGuard ],
-      },
-      {
-        path: NAVIGATIONS.privacy.url,
-        component: MaintenancePageComponent,
-        canActivate: [ AuthGuard ],
-      },
-    ],
-  },
-  {
-    path: NAVIGATIONS.user.url,
-    component: DashboardLayoutComponent,
-    children: [
-      {
-        path: ':username',
-        component: PublicProfilePageComponent,
-        canActivate: [ PublicProfileGuard ],
-      },
-    ],
-  },
-  {
-    path: NAVIGATIONS.settings.url,
-    component: SettingsLayoutComponent,
-    children: [
-      {
-        path: NAVIGATIONS.editTheme.url,
-        component: EditThemePageComponent,
-        canActivate: [ AuthGuard ]
-      },
-    ],
   },
   // otherwise redirect to error
   { path: '**', redirectTo: NAVIGATIONS.notFound.url },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,11 +26,9 @@ import { BrandButtonComponent } from './component/button/brand-button/brand-butt
 import { ButtonComponent } from './component/button/button.component';
 import { EventButtonComponent } from './component/button/event-button/event-button.component';
 import { InformationButtonComponent } from './component/button/information-button/information-button.component';
-import { CalendarComponent } from './component/calendar/calendar.component';
 import { ActivityHistoryCardComponent } from './component/card/activity-history-card/activity-history-card.component';
 import { CardComponent } from './component/card/card.component';
 import { EmployeeCardComponent } from './component/card/employee-card/employee-card.component';
-import { EventCardComponent } from './component/card/event-card/event-card.component';
 import { FeatureCardComponent } from './component/card/feature-card/feature-card.component';
 import { PolicyCardComponent } from './component/card/policy-card/policy-card.component';
 import { PresentationCardComponent } from './component/card/presentation-card/presentation-card.component';
@@ -52,42 +50,21 @@ import { TopSvgComponent } from './component/design/top-svg/top-svg.component';
 import { DropdownMenuComponent } from './component/dropdown-menu/dropdown-menu.component';
 import { ExtLinkComponent } from './component/ext-link/ext-link.component';
 import { HeroComponent } from './component/hero/hero.component';
-import { EventDataComponent } from './component/label/deprecated/event-data/event-data.component';
-import { EventHamburgerMenuComponent } from './component/label/deprecated/event-hamburger-menu/event-hamburger-menu.component';
-import { LabelDataComponent } from './component/label/deprecated/label-data/label-data.component';
-import { LabelWithIconDataComponent } from './component/label/deprecated/label-with-icon-data/label-with-icon-data.component';
 import { LedComponent } from './component/led/led.component';
 import { BottomNavComponent } from './component/nav/bottom-nav/bottom-nav.component';
-import { DashboardLayoutSideNavComponent } from './component/nav/dashboard/dashboard-layout-side-nav/dashboard-layout-side-nav.component';
-import { DashboardLayoutTopNavComponent } from './component/nav/dashboard/dashboard-layout-top-nav/dashboard-layout-top-nav.component';
 import { FooterComponent } from './component/nav/footer/footer.component';
 import { LandingLayoutTopNavComponent } from './component/nav/landing/landing-layout-top-nav/landing-layout-top-nav.component';
 import { CookiePopupComponent } from './component/popup/cookie-popup/cookie-popup.component';
 import { PricePlanComponent } from './component/price-plan/price-plan.component';
 import { ProductPresentationComponent } from './component/product-represantation/product-presentation.component';
-import { RateBarComponent } from './component/rate-bar/rate-bar.component';
-import { SearchComponent } from './component/search/search.component';
-import { SideContentComponent } from './component/side-content/side-content.component';
 import { BusinessPartnerSwiperComponent } from './component/swiper/business-partner-swiper/business-partner-swiper.component';
 import { PresentationSwiperComponent } from './component/swiper/presentation-swiper/presentation-swiper.component';
 import { RequestInterceptor } from './interceptor/request/request.interceptor';
 import { ResponseInterceptor } from './interceptor/response/response.interceptor';
 import { OthComponent } from './oth/oth.component';
-import { CreateEventPageComponent } from './page/dashboard-layout/create-event-page/create-event-page.component';
-import { DashboardLayoutComponent } from './page/dashboard-layout/dashboard-layout.component';
-
-import { DashboardPageComponent } from './page/dashboard-layout/dashboard-page/dashboard-page.component';
-import { EventPageComponent } from './page/dashboard-layout/event-page/event-page.component';
-import { LearnPageComponent } from './page/dashboard-layout/learn-page/learn-page.component';
-import { MyProfilePageComponent } from './page/dashboard-layout/my-profile-page/my-profile-page.component';
-import { PublicProfilePageComponent } from './page/dashboard-layout/public-profile-page/public-profile-page.component';
 import { AboutUsPageComponent } from './page/landing-layout/about-us-page/about-us-page.component';
 import { BrandingPageComponent } from './page/landing-layout/branding-page/branding-page.component';
 import { ContactUsPageComponent } from './page/landing-layout/contact-us-page/contact-us-page.component';
-import { ForgetPasswordPageComponent } from './page/landing-layout/form-pages/forget-password-page/forget-password-page.component';
-import { LoginPageComponent } from './page/landing-layout/form-pages/login-page/login-page.component';
-import { ResetPasswordPageComponent } from './page/landing-layout/form-pages/reset-password-page/reset-password-page.component';
-import { SignUpPageComponent } from './page/landing-layout/form-pages/sign-up-page/sign-up-page.component';
 import { HomePageComponent } from './page/landing-layout/home-page/home-page.component';
 import { LandingLayoutComponent } from './page/landing-layout/landing-layout.component';
 import { CookiePolicyPageComponent } from './page/landing-layout/policy-pages/cookie-policy-page/cookie-policy-page.component';
@@ -97,13 +74,10 @@ import { PricingPageComponent } from './page/landing-layout/pricing-page/pricing
 import { ProductPageComponent } from './page/landing-layout/product-page/product-page.component';
 import { MaintenancePageComponent } from './page/landing-layout/raw-content-pages/maintenance-page/maintenance-page.component';
 import { NotFoundPageComponent } from './page/landing-layout/raw-content-pages/not-found-page/not-found-page.component';
-import { SignUpSuccessPageComponent } from './page/landing-layout/raw-content-pages/sign-up-success-page/sign-up-success-page.component';
-import { VerifyAccountPageComponent } from './page/landing-layout/raw-content-pages/verify-account-page/verify-account-page.component';
 import { SitemapPageComponent } from './page/landing-layout/sitemap-page/sitemap-page.component';
 import { StatusPageComponent } from './page/landing-layout/status-page/status-page.component';
 import { EditThemePageComponent } from './page/settings-layout/edit-theme-page/edit-theme-page.component';
 import { SettingsLayoutComponent } from './page/settings-layout/settings-layout.component';
-import { CallbackPageComponent } from './page/splash-layout/callback-page/callback-page.component';
 import { ExternalRedirectPageComponent } from './page/splash-layout/external-redirect-page/external-redirect-page.component';
 import { SplashLayoutComponent } from './page/splash-layout/splash-layout.component';
 import { CustomersPageComponent } from './page/landing-layout/customers-page/customers-page.component';
@@ -130,42 +104,18 @@ FullCalendarModule.registerPlugins( [
   declarations: [
     OthComponent,
     HomePageComponent,
-    LoginPageComponent,
-    DashboardPageComponent,
-    SignUpPageComponent,
-    SignUpSuccessPageComponent,
-    VerifyAccountPageComponent,
     LandingLayoutComponent,
-    DashboardLayoutComponent,
-    ResetPasswordPageComponent,
-    ForgetPasswordPageComponent,
     LandingLayoutTopNavComponent,
-    DashboardLayoutSideNavComponent,
     BottomNavComponent,
     FooterComponent,
     SplashLayoutComponent,
-    CallbackPageComponent,
-    LabelDataComponent,
-    DashboardLayoutTopNavComponent,
-    LabelWithIconDataComponent,
     NotFoundPageComponent,
-    LearnPageComponent,
     MaintenancePageComponent,
     CookiePopupComponent,
-    CreateEventPageComponent,
     CookiePolicyPageComponent,
     PrivacyPolicyPageComponent,
     TermsPageComponent,
-    PublicProfilePageComponent,
-    EventDataComponent,
     SettingsLayoutComponent,
-    SideContentComponent,
-    MyProfilePageComponent,
-    EventPageComponent,
-    EventHamburgerMenuComponent,
-    CalendarComponent,
-    SearchComponent,
-    RateBarComponent,
     EditThemePageComponent,
     PricingPageComponent,
     PricingCardComponent,
@@ -196,7 +146,6 @@ FullCalendarModule.registerPlugins( [
     PresentationCardComponent,
     HeroComponent,
     EventButtonComponent,
-    EventCardComponent,
     ActivityHistoryCardComponent,
     ContentLoaderComponent,
     ImageContentLoaderComponent,

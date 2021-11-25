90c473d34cc8c1eade9a3d2cbd1cc2114190388c
fatihturker <fatih.turker@optiva.com>
2021-11-26 00:44:25 +0300

    Make map configurable

--- src/app/data/contact-info/contact-info.data.ts
+++ src/app/data/contact-info/contact-info.data.ts
@@ -2,4 +2,5 @@ export const CONTACT_INFO = {
   address: 'Sepapaja 6, 15551 Tallinn, Estonia',
   email: 'info@opentemplatehub.com',
   phone: 'N/A',
+  map: 'https://goo.gl/maps/qKdzpwkhLKuQsaWv6',
 };

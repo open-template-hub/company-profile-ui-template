df589f6a3140cbeb42ef04200ddc59672de6ae26
fatihturker <fatih.turker@optiva.com>
2021-11-26 01:01:49 +0300

    Update Messages

--- src/app/data/contact-info/contact-info.data.ts
+++ src/app/data/contact-info/contact-info.data.ts
@@ -3,6 +3,4 @@ export const CONTACT_INFO = {
   email: 'info@opentemplatehub.com',
   phone: 'N/A',
   map: 'https://goo.gl/maps/qKdzpwkhLKuQsaWv6',
-  embedMapSource:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d129707.39746609103!2d24.59781765497098!3d59.47179210793564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46929499df5616bf%3A0x400b36d18fc6270!2sTallinn%2C%20Estonia!5e0!3m2!1sen!2str!4v1637876159657!5m2!1sen!2str',
 };

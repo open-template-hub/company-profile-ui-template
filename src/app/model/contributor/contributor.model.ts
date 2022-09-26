41d62135963fa2bfdc5371dc70e025653d3c7f39
Furkan Yavuz <furknyavuz@gmail.com>
2022-09-26 15:20:14 +0300

    update contributor card

--- src/app/model/contributor/contributor.model.ts
+++ src/app/model/contributor/contributor.model.ts
@@ -1,7 +1,10 @@
+import { Social } from '../social/social.model';
+
 export interface Contributor {
   profile: {
     name: string;
     photoUri: string;
     level: string;
+    social: Social;
   };
 }

1dc55d1499522a9ea5c7228a3287aa1e8f13f75c
Furkan Yavuz <furknyavuz@gmail.com>
2022-12-19 21:04:34 +0300

    update contributor data

--- src/app/data/contributor/contributor.data.ts
+++ src/app/data/contributor/contributor.data.ts
@@ -15,7 +15,7 @@ export const GOLD_CONTRIBUTORS: Contributor[] = [
     profile: {
       name: 'Kerim Alp Kaya',
       photoUri: 'https://avatars.githubusercontent.com/u/90132495?v=4',
-      level: CONTRIBUTION_LEVELS.gold,
+      level: CONTRIBUTION_LEVELS.diamond,
       social: {
         linkedIn: 'https://www.linkedin.com/in/kerimalpkaya/',
         github: 'https://github.com/kerimalp',
@@ -26,7 +26,7 @@ export const GOLD_CONTRIBUTORS: Contributor[] = [
     profile: {
       name: 'Emre Yurtay',
       photoUri: 'https://avatars.githubusercontent.com/u/111579804?v=4',
-      level: CONTRIBUTION_LEVELS.gold,
+      level: CONTRIBUTION_LEVELS.diamond,
       social: {
         linkedIn: 'https://www.linkedin.com/in/yunus-emre-yurtay-b3745316a/',
         github: 'https://github.com/emreyurtay',
@@ -39,7 +39,7 @@ export const SILVER_CONTRIBUTORS: Contributor[] = [
     profile: {
       name: 'Ulas Arda Burak',
       photoUri: 'https://media-exp1.licdn.com/dms/image/C4E03AQGVNBJ_v3PK5A/profile-displayphoto-shrink_800_800/0/1628159346378?e=1672876800&v=beta&t=8LxzKxKtm40VOTKLWrVVFziBzjBg1kf5UVF_hoVOX94',
-      level: CONTRIBUTION_LEVELS.silver,
+      level: CONTRIBUTION_LEVELS.gold,
       social: {
         linkedIn: 'https://www.linkedin.com/in/ulasardaburak/',
         github: 'https://github.com/Zulk0rne1n',
@@ -52,7 +52,7 @@ export const BRONZE_CONTRIBUTORS: Contributor[] = [
     profile: {
       name: '???',
       photoUri: '',
-      level: CONTRIBUTION_LEVELS.bronze,
+      level: CONTRIBUTION_LEVELS.silver,
       social: {
         linkedIn: 'https://www.linkedin.com/company/open-template-hub',
         github: 'https://github.com/open-template-hub',
